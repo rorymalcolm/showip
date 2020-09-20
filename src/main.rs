9fe0b236d43b72f63315c9ad89f5373398f39938
rorymalcolm <RoryMckenzieMalcolm@gmail.com>
2020-09-20 15:49:54 +0100

    fmt

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     };
     let sockets = getaddrinfo(Some(hostname), Some(servname), Some(hints))
             .unwrap().collect::<std::io::Result<Vec<_>>>().unwrap();
-    print!("IP Addresses:");
+    print!("IP Addresses: ");
     let sockraddr : Vec<String> = sockets.into_iter().map(|x| { x.sockaddr.to_string()}).collect();
     println!("{}", sockraddr.join(", "))
 }
